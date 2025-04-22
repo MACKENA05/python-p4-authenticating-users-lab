354715354a802784d1d69b1ba6b5e61fe6054115
MACKENA05 <mackenamuthui@gmail.com>
2025-04-22 18:57:54 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -47,10 +47,40 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class LogIn(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(),200
+        return {'message':'User not found'},404
+
+class LogOut(Resource):
+    def delete(self):
+        session['user_id']= None
+        return {'message': 'Logged out successfully'}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(),200
+        
+        return {} , 401
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(LogIn, '/login')
+api.add_resource(LogOut, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
